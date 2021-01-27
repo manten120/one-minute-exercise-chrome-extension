294ee93f69f296bed6ff41716dfc1d0cf265d419
stacru <hamaw8888@gmail.com>
2021-01-27 09:18:43 +0900

    F11キー禁止を削除(フルスクリーンのときに禁止できなかったため)

--- contentScript.js
+++ contentScript.js
@@ -4,13 +4,6 @@ $('.img-menus').on('click', function() {
   chrome.runtime.sendMessage('.img-menusがクリックされました')
 });
 
-// F11キー禁止
-$(document).keydown(function(e){
-  if(e.key === 'F11'){
-      return false;
-  }
-});
-
 // 右クリック禁止
 $(document).on('contextmenu', function() {
   return false;
